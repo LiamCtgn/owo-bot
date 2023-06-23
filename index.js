53745e3bcd582d42f83efd2b4a026104f54cfacd
LongAKolangle <thanhlong0975855842@gmail.com>
2023-06-23 13:17:59 +0700

    fixed auto solve captcha

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ process.on("SIGINT", function () {
     .on("shardReady", () => reloadPresence(client))
     .on("messageCreate", async (message) => {
         if(message.author.id == global.owoID) {
-            if((message.content.includes(global.displayName) && message.content.match(/(check|verify) that you are.{1,3}human!/igm)) || (message.content.includes('Beep Boop') && message.channel.type == 'DM')) {
+            if(((message.content.includes(m.client.user.username) || message.content.includes(global.displayName)) && message.content.match(/(check|verify) that you are.{1,3}human!/igm)) || (message.content.includes('Beep Boop') && message.channel.type == 'DM')) {
                 global.captchaDetected = true;
                 console.log("\n");
                 console.log("\x1b[92mTotal command sent: \x1b[0m" + global.totalcmd);
@@ -78,7 +78,7 @@ process.on("SIGINT", function () {
                 console.log("\x1b[92mTotal active time: \x1b[0m" + timeHandler(global.startTime, Date.now()));
                 console.log("\x1b[36mSELFBOT HAS BEEN TERMINATED!\x1b[0m");
 
-                if(!global.config.autoWait && !global.config.captchaAPI) process.exit(1);
+                if(!global.config.autoResume && !global.config.captchaAPI) process.exit(1);
 
                 else if([1, 2].includes(global.config.captchaAPI)) {
                     if(message.attachments) {
@@ -110,7 +110,8 @@ process.on("SIGINT", function () {
             }
 
             else if(message.content.match(/verified that you are.{1,3}human!/igm) && message.channel.type == 'DM') {
-                log("CAPTCHA HAS BEEN RESOLVED, RESTARTING SELFBOT...", "i");
+                log(`CAPTCHA HAS BEEN RESOLVED${global.config.autoResume ? ", RESTARTING SELFBOT..." : ""}`, "i");
+                if(!global.autoResume) process.exit(1);
                 global.captchaDetected = false;
                 main();
             }
